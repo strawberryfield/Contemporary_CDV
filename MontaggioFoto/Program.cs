b5cbde69601120af6fc7b735a674ebe8c18be893
roberto ceccarelli <roberto.strawberryfield@gmail.com>
2020-12-13 16:56:31 +0100

    Accorpamento immagini

--- MontaggioFoto/Program.cs
+++ MontaggioFoto/Program.cs
@@ -18,10 +18,52 @@
 // along with CasaSoft CCDV Tools.  
 // If not, see <http://www.gnu.org/licenses/>.
 
-using ImageMagick;
 using Casasoft.CCDV;
+using ImageMagick;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 Formats fmt = new(300);
 Images img = new(fmt);
 
-MagickImage final = img.FineArt10x15_o();
+List<string> files = new();
+for(int p=0; p<args.Length; p++)
+{
+    string filename = args[p];
+    files.AddRange(Directory.GetFiles(Path.GetDirectoryName(filename), Path.GetFileName(filename)).ToList());
+}
+
+for(int i = 0; i < files.Count; i++)
+{
+    MagickImage final = img.FineArt10x15_o();
+    Console.WriteLine($"Processing: {files[i]}");
+    MagickImage img1 = Utils.ResizeAndFill(new(files[i]), fmt.CDV_Internal_v);
+
+    MagickImage img2;
+    i++;
+    if(i < files.Count)
+    {
+        Console.WriteLine($"Processing: {files[i]}");
+        img2 = Utils.ResizeAndFill(new(files[i]), fmt.CDV_Internal_v);
+    }
+    else
+    {
+        img2 = img.CDV_Internal_v();
+    }
+
+    final.Composite(HalfCard(img1), Gravity.West);
+    final.Composite(HalfCard(img2), Gravity.East);
+
+    final.Write($"card{(i+1)/2,3:D3}.jpg");
+}
+
+MagickImage HalfCard(MagickImage img)
+{
+    img.BorderColor = MagickColors.Black;
+    img.Border(1);
+    MagickImage half = new(MagickColors.White, fmt.FineArt10x15_o.Width / 2, fmt.FineArt10x15_o.Height);
+    half.Composite(img, Gravity.Center);
+    return half;
+}
