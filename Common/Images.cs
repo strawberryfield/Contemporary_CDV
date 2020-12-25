9700300675d4e029620b96e0742cfbf4014767bb
roberto ceccarelli <roberto.strawberryfield@gmail.com>
2020-12-25 12:16:57 +0100

    case

--- Common/Images.cs
+++ Common/Images.cs
@@ -1,21 +1,21 @@
-﻿// copyright (c) 2020 Roberto Ceccarelli - CasaSoft
+﻿// copyright (c) 2020 Roberto Ceccarelli - Casasoft
 // http://strawberryfield.altervista.org 
 // 
-// This file is part of CasaSoft Contemporary Carte de Visite Tools
+// This file is part of Casasoft Contemporary Carte de Visite Tools
 // 
-// CasaSoft CCDV Tools is free software: 
+// Casasoft CCDV Tools is free software: 
 // you can redistribute it and/or modify it
 // under the terms of the GNU Affero General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 // 
-// CasaSoft CCDV Tools is distributed in the hope that it will be useful,
+// Casasoft CCDV Tools is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
 // See the GNU General Public License for more details.
 // 
 // You should have received a copy of the GNU AGPL v.3
-// along with CasaSoft CCDV Tools.  
+// along with Casasoft CCDV Tools.  
 // If not, see <http://www.gnu.org/licenses/>.
 
 using ImageMagick;
