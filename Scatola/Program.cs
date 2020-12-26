d4e6c746c72df4768d9d1b6cb04e7b2af258fd42
roberto ceccarelli <roberto.strawberryfield@gmail.com>
2020-12-26 21:10:19 +0100

    Box builder

---
+++ Scatola/Program.cs
@@ -0,0 +1,84 @@
+﻿// copyright (c) 2020 Roberto Ceccarelli - Casasoft
+// http://strawberryfield.altervista.org 
+// 
+// This file is part of Casasoft Contemporary Carte de Visite Tools
+// 
+// Casasoft CCDV Tools is free software: 
+// you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Casasoft CCDV Tools is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU AGPL v.3
+// along with Casasoft CCDV Tools.  
+// If not, see <http://www.gnu.org/licenses/>.
+
+using Casasoft.CCDV;
+using ImageMagick;
+using Mono.Options;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+#region command line
+bool shouldShowHelp = false;
+string outputName = "box";
+string dpi = "300";
+string thickness = "5";
+
+string exeName = Path.GetFileNameWithoutExtension(AppDomain.CurrentDomain.FriendlyName);
+Utils.WelcomeBanner(exeName);
+
+OptionSet options = new OptionSet
+{
+    { "t|thickness=", "set the box thickness (default 5mm)", t => thickness = t },
+    { "o|output=", "set output filename (default 'box')", o => outputName = o },
+    { "dpi=", "set output resolution (default 300)", res => dpi = res },
+    { "h|help", "show this message and exit", h => shouldShowHelp = h != null },
+};
+
+List<string> filesList;
+try
+{
+    // parse the command line
+    filesList = options.Parse(args);
+}
+catch (OptionException e)
+{
+    Utils.ShowParametersError(exeName, e);
+    return;
+}
+
+if (shouldShowHelp)
+    Utils.ShowHelp(exeName, "[option]+", options);
+#endregion
+
+int nthickness;
+if (!Int32.TryParse(thickness, out nthickness))
+{
+    Console.Error.WriteLine($"Incorrect thickness value '{thickness}'. Using default value.");
+    nthickness = 5;
+}
+
+int ndpi;
+if (!Int32.TryParse(dpi, out ndpi))
+{
+    Console.Error.WriteLine($"Incorrect dpi value '{dpi}'. Using default value.");
+    ndpi = 300;
+}
+
+Formats fmt = new(ndpi);
+Images img = new(fmt);
+MagickImage output = img.InCartha20x27_o();
+ScatolaBuilder sc = new(nthickness, fmt);
+
+// only for test 
+sc.CreateTestImages();
+
+output.Composite(sc.Build(), Gravity.Center);
+output.Write($"{outputName}.jpg");
---
+++ Scatola/ScatolaBuilder.cs
@@ -0,0 +1,196 @@
+﻿// copyright (c) 2020 Roberto Ceccarelli - Casasoft
+// http://strawberryfield.altervista.org 
+// 
+// This file is part of Casasoft Contemporary Carte de Visite Tools
+// 
+// Casasoft CCDV Tools is free software: 
+// you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Casasoft CCDV Tools is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU AGPL v.3
+// along with Casasoft CCDV Tools.  
+// If not, see <http://www.gnu.org/licenses/>.
+
+using ImageMagick;
+
+namespace Casasoft.CCDV
+{
+    public class ScatolaBuilder
+    {
+        private int spessore;
+        private Formats fmt;
+
+        #region constructors
+        public ScatolaBuilder(int Spessore, int dpi) : this(Spessore, new Formats(dpi)) { }
+
+        public ScatolaBuilder(int Spessore, Formats formats)
+        {
+            fmt = formats;
+            spessore = fmt.ToPixels(Spessore);
+            makeEmptyImages();
+        }
+
+        public ScatolaBuilder(int Spessore) : this(Spessore, 300) { }
+        public ScatolaBuilder() : this(5) { }
+        #endregion
+
+        private MagickGeometry topFormat;
+        private MagickGeometry borderFormat;
+        private MagickGeometry frontFormat;
+
+        private MagickImage topImage;
+        private MagickImage bottomImage;
+        private MagickImage leftImage;
+        private MagickImage rightImage;
+        private MagickImage frontImage;
+        private MagickImage backImage;
+
+
+        private void makeEmptyImages()
+        {
+            frontFormat = fmt.CDV_Full_v;
+            frontFormat.Width += fmt.ToPixels(5);
+            frontFormat.Height += fmt.ToPixels(5);
+
+            borderFormat = new(spessore, frontFormat.Height);
+            topFormat = new(frontFormat.Width, spessore);
+
+            topImage = new(MagickColors.White, topFormat.Width, topFormat.Height);
+            bottomImage = new(MagickColors.White, topFormat.Width, topFormat.Height);
+
+            leftImage = new(MagickColors.White, borderFormat.Width, borderFormat.Height);
+            rightImage = new(MagickColors.White, borderFormat.Width, borderFormat.Height);
+
+            frontImage = new(MagickColors.White, frontFormat.Width, frontFormat.Height);
+            backImage = new(MagickColors.White, frontFormat.Width, frontFormat.Height);
+        }
+
+        public void SetTopImage(string filename) => topImage = Utils.RotateResizeAndFill(new(filename), topFormat);
+        public void SetBottomImage(string filename) => bottomImage = Utils.RotateResizeAndFill(new(filename), topFormat);
+        public void SetLeftImage(string filename) => leftImage = Utils.RotateResizeAndFill(new(filename), borderFormat);
+        public void SetRightImage(string filename) => rightImage = Utils.RotateResizeAndFill(new(filename), borderFormat);
+        public void SetFrontImage(string filename) => frontImage = Utils.RotateResizeAndFill(new(filename), frontFormat);
+        public void SetBackImage(string filename) => backImage = Utils.RotateResizeAndFill(new(filename), frontFormat);
+
+        public MagickImage Build()
+        {
+            MagickImage ret = new(MagickColors.White,
+                frontFormat.Width * 2 + spessore * 2 + fmt.ToPixels(10) + 2,
+                frontFormat.Height + spessore * 2 + fmt.ToPixels(20) + 2);
+
+
+            // Aggiunge le alette ai bordi della scatola
+            MagickImage LeftBorderWithExtra = new(MagickColors.White,
+                borderFormat.Width, borderFormat.Height + fmt.ToPixels(20));
+            Drawables draw = new();
+            draw.StrokeColor(MagickColors.Black).StrokeWidth(1);
+            draw = cap(draw, spessore, fmt.ToPixels(10));
+            draw.Draw(LeftBorderWithExtra);
+            LeftBorderWithExtra.Rotate(180);
+            draw.Draw(LeftBorderWithExtra);
+
+            MagickImage RightBorderWithExtra = (MagickImage)LeftBorderWithExtra.Clone();
+            LeftBorderWithExtra.Composite(leftImage, Gravity.Center);
+            RightBorderWithExtra.Composite(rightImage, Gravity.Center);
+
+
+            // Aggiungiamo coperchi e lembi di chiusura
+            MagickImage BackWithTop = new(MagickColors.White,
+                frontFormat.Width, frontFormat.Height + spessore + fmt.ToPixels(10));
+            draw = new();
+            draw.StrokeColor(MagickColors.Black).StrokeWidth(1);
+            cap(draw, frontFormat.Width, fmt.ToPixels(10)).Draw(BackWithTop);
+
+            MagickImage FrontWithBottom = (MagickImage)BackWithTop.Clone();
+            FrontWithBottom.Rotate(180);
+
+            FrontWithBottom.Composite(frontImage, Gravity.North);
+            FrontWithBottom.Composite(bottomImage, Gravity.South, new PointD(0, fmt.ToPixels(10)));
+            BackWithTop.Composite(backImage, Gravity.South);
+            topImage.Rotate(180);
+            BackWithTop.Composite(topImage, Gravity.North, new PointD(0, fmt.ToPixels(10)));
+            MagickImage frontTopImage = (MagickImage)frontImage.Clone();
+            frontTopImage.Crop(frontFormat.Width, fmt.ToPixels(10));
+            frontTopImage.Rotate(180);
+            BackWithTop.Composite(frontTopImage, Gravity.North);
+
+
+            // Margini di taglio
+            draw = new();
+            draw.StrokeColor(MagickColors.Black).StrokeWidth(1);
+            int bordertop = spessore + fmt.ToPixels(10);
+            int borderbottom = bordertop + +frontFormat.Height;
+            int marginright = ret.Width - 1;
+            draw.Line(0, bordertop - spessore, 0, borderbottom);
+            draw.Line(marginright, bordertop, marginright, borderbottom);
+            draw.Line(marginright - fmt.ToPixels(10), bordertop, marginright, bordertop);
+            draw.Line(marginright - fmt.ToPixels(10), borderbottom, marginright, borderbottom);
+            draw.Line(0, borderbottom, frontFormat.Width, borderbottom);
+            draw.Line(marginright - fmt.ToPixels(10) - spessore, bordertop,
+                marginright - fmt.ToPixels(10) - spessore - frontFormat.Width, bordertop);
+            draw.Draw(ret);
+
+            // Infine assembliamo tutto
+            ret.Composite(BackWithTop, Gravity.Northwest, new PointD(1, 0));
+            ret.Composite(LeftBorderWithExtra, Gravity.West, new PointD(1 + frontFormat.Width, 0));
+            ret.Composite(FrontWithBottom, Gravity.Southwest, new PointD(1 + frontFormat.Width + borderFormat.Width, 0));
+            ret.Composite(RightBorderWithExtra, Gravity.West, new PointD(1 + frontFormat.Width * 2 + borderFormat.Width, 0));
+
+            return ret;
+        }
+
+        private Drawables cap(Drawables d, int w, int h) =>
+            (Drawables)d.Line(0, h - 1, 0, 0).Line(w - 1, h - 1, w - 1, 0).Line(0, 0, w - 1, 0);
+
+        #region test
+        public void CreateTestImages()
+        {
+            frontImage = new(MagickColors.LightGray, frontFormat.Width, frontFormat.Height);
+            Drawables draw = new();
+            draw.StrokeColor(MagickColors.Black).FontPointSize(120).TextAlignment(TextAlignment.Center);
+            draw.Text(frontImage.Width / 2, frontImage.Height / 2, "Front");
+            draw.FontPointSize(50).Text(frontImage.Width / 2, fmt.ToPixels(6), "Front top");
+            draw.Draw(frontImage);
+
+            backImage = new(MagickColors.LightBlue, frontFormat.Width, frontFormat.Height);
+            draw = new();
+            draw.StrokeColor(MagickColors.Black).FontPointSize(120).TextAlignment(TextAlignment.Center);
+            draw.Text(frontFormat.Width / 2, frontFormat.Height / 2, "Back");
+            draw.Draw(backImage);
+
+            topImage = new(MagickColors.LightGreen, topFormat.Width, topFormat.Height);
+            draw = new();
+            draw.StrokeColor(MagickColors.Black).FontPointSize(30).TextAlignment(TextAlignment.Center);
+            draw.Text(topFormat.Width / 2, topFormat.Height / 2, "Top");
+            draw.Draw(topImage);
+
+            bottomImage = new(MagickColors.LightCoral, topFormat.Width, topFormat.Height);
+            draw = new();
+            draw.StrokeColor(MagickColors.Black).FontPointSize(30).TextAlignment(TextAlignment.Center);
+            draw.Text(topFormat.Width / 2, topFormat.Height / 2, "Bottom");
+            draw.Draw(bottomImage);
+
+            leftImage = new(MagickColors.LightYellow, borderFormat.Height, borderFormat.Width);
+            draw = new();
+            draw.StrokeColor(MagickColors.Black).FontPointSize(30).TextAlignment(TextAlignment.Center);
+            draw.Text(borderFormat.Height / 2, borderFormat.Width / 2, "Left");
+            draw.Draw(leftImage);
+            leftImage.Rotate(90);
+
+            rightImage = new(MagickColors.Linen, borderFormat.Height, borderFormat.Width);
+            draw = new();
+            draw.StrokeColor(MagickColors.Black).FontPointSize(30).TextAlignment(TextAlignment.Center);
+            draw.Text(borderFormat.Height / 2, borderFormat.Width / 2, "Right");
+            draw.Draw(rightImage);
+            rightImage.Rotate(90);
+        }
+        #endregion
+    }
+}
