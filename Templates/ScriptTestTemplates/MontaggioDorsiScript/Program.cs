928be5f674ec6a0e0781ae3d37efd852fdb5ea05
roberto ceccarelli <roberto.strawberryfield@gmail.com>
2022-03-29 21:29:08 +0200

    Script Test Templates

---
+++ Templates/ScriptTestTemplates/MontaggioDorsiScript/Program.cs
@@ -0,0 +1,40 @@
+﻿// Sample template for Casasoft Contemporary Carte de Visite Script
+// See https://strawberryfield.altervista.org/carte_de_visite for more information
+
+// These are the usings included when compiling the script at run-time
+using Casasoft.CCDV;
+using Casasoft.CCDV.Engines;
+using Casasoft.CCDV.JSON;
+using ImageMagick;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+MontaggioDorsiEngine eng = new();
+UserScript user = new(eng);
+
+user.ProcessOnLoad(new MagickImage());
+
+public class UserScript
+{
+    private MontaggioDorsiEngine engine;
+	
+    public UserScript(IEngine eng) 
+    {
+        engine = (MontaggioDorsiEngine)eng;
+        System.Reflection.MethodInfo m = this.GetType().GetMethod("Init");
+        if (m != null) m.Invoke(this, null);
+    }
+
+    // vvv --- put you script here --- vvv
+
+    /// <summary>
+    /// Preprocesses the loaded image
+    /// </summary>
+    /// <param name=""image"">The loaded image</param>
+    /// <returns>The Processed image</returns>
+    public MagickImage ProcessOnLoad(MagickImage image) => image;
+
+    // ^^^ --- script ends here --- ^^^
+
+}
