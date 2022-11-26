9eefc260552522bb8ef5ea90c851610cad26ce29
roberto ceccarelli <roberto.strawberryfield@gmail.com>
2022-11-26 18:31:16 +0100

    more controls on CubettiForm

--- CCDV/Forms/CubettiForm.xaml.cs
+++ CCDV/Forms/CubettiForm.xaml.cs
@@ -1,27 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿// copyright (c) 2020-2022 Roberto Ceccarelli - Casasoft
+// http://strawberryfield.altervista.org 
+// 
+// This file is part of Casasoft Contemporary Carte de Visite Tools
+// https://github.com/strawberryfield/Contemporary_CDV
+// 
+// Casasoft CCDV Tools is free software: 
+// you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Casasoft CCDV Tools is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU AGPL v.3
+// along with Casasoft CCDV Tools.  
+// If not, see <http://www.gnu.org/licenses/>.
 
-namespace Casasoft.CCDV.UI
+using Casasoft.CCDV.Engines;
+
+namespace Casasoft.CCDV.UI;
+
+/// <summary>
+/// Interaction logic for CubettiForm.xaml
+/// </summary>
+public partial class CubettiForm : BaseMultipageForm
 {
-    /// <summary>
-    /// Interaction logic for CubettiForm.xaml
-    /// </summary>
-    public partial class CubettiForm : BaseMultipageForm
+    public CubettiForm() : base()
     {
-        public CubettiForm() : base()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+        engine = new CubettiEngine();
+    }
+
+    protected override void setEngineParameters()
+    {
+        base.setEngineParameters();
+        CubettiEngine eng = (CubettiEngine)engine;
+        addAllFiles();
+        eng.FillColor = commonOptions.FillColor;
+        eng.BorderColor = commonOptions.BorderColor;
+        eng.Dpi = commonOptions.DpiValue;
+        eng.Script = commonOptions.Script;
+        eng.Tag = commonOptions.ScriptTag;
+        eng.PaperFormat = paperFormat.PaperFormat;
+        eng.Rows = txtRows.Value;
+        eng.Columns = txtCols.Value;
+        eng.Size = txtSize.Value;
+    }
+
+    protected override void loadJson(string json)
+    {
+        base.loadJson(json);
+        CubettiEngine eng = (CubettiEngine)engine;
+        commonOptions.FillColor = eng.FillColor;
+        commonOptions.BorderColor = eng.BorderColor;
+        commonOptions.DpiValue = eng.Dpi;
+        commonOptions.ScriptTag = eng.Tag;
+        paperFormat.PaperFormat = eng.PaperFormat;
+        txtRows.Value = eng.Rows;
+        txtCols.Value = eng.Columns;
+        txtSize.Value = eng.Size;
+
+        addFile(1, filename1);
+        addFile(2, filename2);
+        addFile(3, filename3);
+        addFile(4, filename4);
+        addFile(5, filename5);
+        addFile(6, filename6);
     }
 }
